51b1f8bc18b79c67a35b89a68fa85a4ef52b257d
unknown <midots202@gmail.com>
2021-12-03 18:38:16 +0200

    1

--- 102-110/1/main.js
+++ 102-110/1/main.js
@@ -1,1 +1,13 @@
-HI gitasdgit a
+let numbers = prompt("Print Number From – To", "5-20").split("-");
+
+if (+numbers[0] < +numbers[1]) {
+    for (i = +numbers[0]; i <= +numbers[1]; i++) {
+        console.log(+numbers[0]);
+        +numbers[0]++;
+    }
+} else if (+numbers[0] > +numbers[1]) {
+    for (i = +numbers[1]; i <= +numbers[0]; i++) {
+        console.log(+numbers[1]);
+        +numbers[1]++;
+    }
+}
