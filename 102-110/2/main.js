f2ee68f461dde4d89fc349202ce910bc08cd9563
unknown <midots202@gmail.com>
2021-12-03 20:40:09 +0200

    T

--- 102-110/2/main.js
+++ 102-110/2/main.js
@@ -4,11 +4,12 @@ function alert() {
     let span = document.createElement("span");
     let exit = document.createElement("div");
 
+    document.body.style.cssText = "display:grid; place-content: center;";
     div.style.cssText =
         "background-color : #F5F3F4; width: 400px; height: 150px; display: flex; justify-content: center; alighn-items: center; flex-direction: column; text-align: center";
     h1.style.cssText = "margin-top: 0";
     exit.style.cssText =
-        "position: relative; top: -110px; right: -200px; width: 50px; height: 10px; border-radius: 50%; background-color: red";
+        "position: relative; top: -110px; right: -385px; width: 20px; height: 20px; border-radius: 50%; background-color: red";
     h1.innerText = "Welcome";
     span.innerText = "Welcome To Elzero Web School";
     exit.innerText = "X";
@@ -16,5 +17,12 @@ function alert() {
     div.appendChild(span);
     div.appendChild(exit);
     document.body.appendChild(div);
+
+    exit.onclick = function () {
+        div.style.cssText = "display:none";
+    };
 }
-alert();
+
+setTimeout(() => {
+    alert();
+}, 5000);
