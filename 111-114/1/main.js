e44d6f6ea7e3470a1d799948066a34b6966f4f89
unknown <midots202@gmail.com>
2021-12-08 19:17:07 +0200

    a

--- 111-114/1/main.js
+++ 111-114/1/main.js
@@ -1,1 +1,34 @@
-شسي
+if(!window.localStorage.getItem("fontFamily")){
+    window.localStorage.setItem("fontFamily","Open Sans")
+    window.localStorage.setItem("color","black")
+    window.localStorage.setItem("fontSize","16px")
+}
+
+let para = document.querySelector("p");
+
+function setLocal (localName,localValue){
+    window.localStorage.setItem(localName,localValue)
+}
+
+let fontfamily = document.querySelector("#fontFamily")
+fontfamily.onchange = function (){
+    setLocal("fontFamily",fontfamily.value)
+    para.style.fontFamily = window.localStorage.getItem("fontFamily")
+}
+let color = document.querySelector("#color")
+color.onchange = function (){
+    setLocal("color",color.value)
+    para.style.color = window.localStorage.getItem("color")
+}
+let fontSize = document.querySelector("#font-size")
+fontSize.onchange = function (){
+    setLocal("fontSize",fontSize.value)
+    para.style.fontSize = window.localStorage.getItem("fontSize")
+}
+fontfamily.value = window.localStorage.getItem("fontFamily")
+color.value = window.localStorage.getItem("color")
+fontSize.value = window.localStorage.getItem("fontSize")
+
+para.style.fontFamily = window.localStorage.getItem("fontFamily")
+para.style.color = window.localStorage.getItem("color")
+para.style.fontSize = window.localStorage.getItem("fontSize")
