e9bb7ae37847713952773a01f68eee36efc29252
unknown <midots202@gmail.com>
2021-12-13 23:43:10 +0200

    A

--- 111-114/Challange/main.js
+++ 111-114/Challange/main.js
@@ -1,27 +1,56 @@
 let inputTask = document.querySelector(".add input")
 let addTask = document.querySelector(".add button")
 let tasksList = document.querySelector(".tasks")
-
 let arrayOfTasks = [];
 
-function createTask(taskText) {
+function loadTasks() {
+    if(window.localStorage.getItem("Tasks")){
+        arrayOfTasks = JSON.parse(window.localStorage.getItem("Tasks"))
+        for(let i = 0; i < arrayOfTasks.length; i++){
+            createTask(arrayOfTasks[i].title,arrayOfTasks[i].id)
+        }
+    }
+}
+loadTasks()
+
+function createTask(taskText,taskid) {
     let taskDiv = document.createElement("div")
     taskDiv.classList.add("task")
-    let taskSpan = document.createElement("span")
-    taskSpan.innerText = taskText
-    taskDiv.appendChild(taskSpan)
+    taskDiv.setAttribute("task-Id",taskid)
+    taskDiv.innerText = taskText
+    taskDiv.setAttribute("id","task")
     let taskDeleteButton = document.createElement("button")
     taskDeleteButton.innerText = "Delete"
-    taskSpan.appendChild(taskDeleteButton)
+    taskDeleteButton.setAttribute("task-Id",taskid)
+    taskDeleteButton.classList.add("delete")
+    taskDiv.appendChild(taskDeleteButton)
     tasksList.appendChild(taskDiv)
 }
 
-function createLocalStorage(params) {
-    
+function addTaskToTheArray(taskname) {
+    let dateId = Date.now();
+    arrayOfTasks.push({id:dateId ,title: taskname,status: false})
+    window.localStorage.setItem("Tasks",JSON.stringify(arrayOfTasks))
+    createTask(inputTask.value,dateId)
 }
 
 addTask.onclick = function () {
     if(inputTask.value !== ""){
-    createTask(inputTask.value)
+    addTaskToTheArray(inputTask.value)
     inputTask.value = ""
 }}
+
+document.addEventListener("click",function (e) {
+    if (e.target.className === "delete"){
+        let task = arrayOfTasks.filter(function (ele){
+            return ele.id != e.target.parentElement.getAttribute("task-id")
+        })
+        arrayOfTasks = task
+        window.localStorage.setItem("Tasks",JSON.stringify(arrayOfTasks))
+        tasksList.innerHTML = ""
+        loadTasks()
+    }
+    if(e.target.getAttribute("id") == "task"){
+        e.target.classList.toggle("deactivate")
+    }
+})
